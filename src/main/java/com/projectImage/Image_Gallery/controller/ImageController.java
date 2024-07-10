e0a8844c0771e6226f16907aef5470a050c7d566
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-10 11:17:16 +0200

    fix: change api url so it matches frontent endpoin

--- src/main/java/com/projectImage/Image_Gallery/controller/ImageController.java
+++ src/main/java/com/projectImage/Image_Gallery/controller/ImageController.java
@@ -5,11 +5,13 @@ import com.projectImage.Image_Gallery.services.ImageServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 @CrossOrigin(origins = "*")
 public class ImageController {
-    String API_BASE_URL = "http://localhost:8080/api/v1";
+
 
     @Autowired
     ImageServices imageService;
