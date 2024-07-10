228db2518ec1631a1e4da0b5d17eea3257635382
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-10 10:09:14 +0200

    feat: add injection of Crud services interface through iRepository

--- src/main/java/com/projectImage/Image_Gallery/services/ImageServices.java
+++ src/main/java/com/projectImage/Image_Gallery/services/ImageServices.java
@@ -1,10 +1,14 @@
 package com.projectImage.Image_Gallery.services;
 
 import com.projectImage.Image_Gallery.repositories.IimageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class ImageServices {
+
+    @Autowired
     IimageRepository iimageRepository;
-    public void deleteImage(long id) {
-        iimageRepository.deleteById(id);
-    }
+
 }
