9f10e0bc3909e2b7b706e0f4075985aaddafecd5
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-10 10:10:48 +0200

    feat: add mockito annotations in controller tests

--- src/test/java/com/projectImage/Image_Gallery/ControllerTests.java
+++ src/test/java/com/projectImage/Image_Gallery/ControllerTests.java
@@ -1,4 +1,26 @@
 package com.projectImage.Image_Gallery;
 
+
+import com.projectImage.Image_Gallery.controller.ImageController;
+import com.projectImage.Image_Gallery.services.ImageServices;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+
+@SpringBootTest
 public class ControllerTests {
+
+    @Mock
+    private ImageServices imageServices;
+
+    @InjectMocks
+    private ImageController imageController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 }
