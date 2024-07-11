80203487e9746b7c1feab018fada6b74ef5d21a8
jmat732 <jmatisamuel@gmail.com>
2024-07-11 20:55:58 +0200

    Chnages names

--- src/main/java/com/projectImage/Image_Gallery/controller/ImageController.java
+++ src/main/java/com/projectImage/Image_Gallery/controller/ImageController.java
@@ -1,16 +1,21 @@
 package com.projectImage.Image_Gallery.controller;
 
-import com.projectImage.Image_Gallery.models.Image;
-import com.projectImage.Image_Gallery.services.ImageServices;
-
 import java.util.List;
 
 import javax.management.InstanceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+import com.projectImage.Image_Gallery.models.Image;
+import com.projectImage.Image_Gallery.services.ImageServices;
 
 @RestController
 @RequestMapping("/api/v1")
--- src/main/java/com/projectImage/Image_Gallery/services/ImageServices.java
+++ src/main/java/com/projectImage/Image_Gallery/services/ImageServices.java
@@ -1,28 +1,21 @@
 package com.projectImage.Image_Gallery.services;
 
-<<<<<<< HEAD
-import com.projectImage.Image_Gallery.models.Image;
-import com.projectImage.Image_Gallery.repositories.IImageRepository;
-=======
-
 import java.util.List;
 import java.util.Optional;
 
 import javax.management.InstanceNotFoundException;
->>>>>>> origin/feature/read
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projectImage.Image_Gallery.models.Image;
-import com.projectImage.Image_Gallery.repositories.IimageRepository;
+import com.projectImage.Image_Gallery.repositories.IImageRepository;
 
 
 @Service
 public class ImageServices {
 
     @Autowired
-<<<<<<< HEAD
     IImageRepository iImageRepository;
 
 
@@ -32,17 +25,17 @@ public class ImageServices {
 
     public Image createImage(Image newImage){
         return iImageRepository.save(newImage);
-=======
-    IimageRepository IimageRepository;
+    }
+   
 
     public List<Image> getAllImages(){
-        return (List<Image>) IimageRepository.findAll();
+        return (List<Image>) iImageRepository.findAll();
     }
 
     public Image getImageById(Long id) throws InstanceNotFoundException {
-        Optional<Image> image = IimageRepository.findById(id);
+        Optional<Image> image = iImageRepository.findById(id);
         return image.orElseThrow(() -> new InstanceNotFoundException("Image not found with id " + id));
->>>>>>> origin/feature/read
     }
 
 }
+
