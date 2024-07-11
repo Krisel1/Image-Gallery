5ba2aa53649933572f2dbc6e9f467403b8a21a8d
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-11 12:44:37 +0200

    refactor: fix name of class and file in repository so it fits convenction

--- src/main/java/com/projectImage/Image_Gallery/repositories/IimageRepository.java
+++ src/main/java/com/projectImage/Image_Gallery/repositories/IimageRepository.java
@@ -2,5 +2,5 @@ package com.projectImage.Image_Gallery.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface IimageRepository extends CrudRepository {
+public interface IImageRepository extends CrudRepository {
 }
