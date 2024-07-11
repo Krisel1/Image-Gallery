f1750399f6bc5bd364f5304fb797aa208d116fd4
jmat732 <jmatisamuel@gmail.com>
2024-07-11 11:31:47 +0200

    throws InstanceNotFoundExceptiont for search id in service

--- src/main/java/com/projectImage/Image_Gallery/services/ImageServices.java
+++ src/main/java/com/projectImage/Image_Gallery/services/ImageServices.java
@@ -1,10 +1,13 @@
 package com.projectImage.Image_Gallery.services;
 
+
 import java.util.List;
+import java.util.Optional;
+
+import javax.management.InstanceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.projectImage.Image_Gallery.models.Image;
 import com.projectImage.Image_Gallery.repositories.IimageRepository;
@@ -20,10 +23,9 @@ public class ImageServices {
         return (List<Image>) IimageRepository.findAll();
     }
 
-    public Image getImageById(@PathVariable Long id) {
-        return IimageRepository.findById(id).orElse(null);
+    public Image getImageById(Long id) throws InstanceNotFoundException {
+        Optional<Image> image = IimageRepository.findById(id);
+        return image.orElseThrow(() -> new InstanceNotFoundException("Image not found with id " + id));
     }
-
-
 
 }
